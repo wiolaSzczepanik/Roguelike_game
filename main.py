1cb83f440944db8b48f44ddde6d5794b240f109f
wiolaSzczepanik <wiolaszcz@gmail.com>
2017-09-04 19:56:10 +0200

    Add function to print title of game

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
-def main():
-    skills = {}
-    equipment = []
-    title_screen()
-    story_screen()
-    next_screen(arg)
-
-    create_character_screen()
-    next_screen()
-
-    how_to_play()
-    next_screen()
-
-    print_board(board1, skills)
-    next_screen()
-    print_board(board2, skills)
-    next_screen()
-    win()
+def intro_title_game(filename):
+    game_title = open(filename, "r")
+    print_game_name = game_title.read()
+    print(print_game_name)
+    game_title.close()
 
 
 def print_board(board, skills):
     move()
     take_item(item, skills)
     fight(live, skills)
+
+
+def main():
+    intro_title_game()
+    # skills = {}
+    # equipment = []
+    # title_screen()
+    # story_screen()
+    # next_screen(arg)
+    #
+    # create_character_screen()
+    # next_screen()
+    #
+    # how_to_play()
+    # next_screen()
+    #
+    # print_board(board1, skills)
+    # next_screen()
+    # print_board(board2, skills)
+    # next_screen()
+    # win()
